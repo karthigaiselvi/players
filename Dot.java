8de3cdd673dd669e7816495ec01002693bd28051
karthigaiselvi <35292744+karthigaiselvi@users.noreply.github.com>
2018-08-01 11:27:31 +0530

    Create Dot.java

---
+++ Dot.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Dot
+{
+    public static void main(String ar[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.print(str+".");
+    }
+}
